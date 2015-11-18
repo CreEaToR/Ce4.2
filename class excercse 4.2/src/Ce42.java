b36a5d1e755c71ba76acaa56d9d7ced95891ff9a
Jm21007 <Jm21007@AN112345-CT122.nvstu.edu>
2015-11-18 13:11:06 -0500

    Initial

---
+++ class excercse 4.2/src/Ce42.java
@@ -0,0 +1,60 @@
+
+public class Ce42 {
+	
+	
+	public static void main(String args[]){
+		
+		int num = 100;
+		
+	
+		
+		
+		for(int i = 0; i<num;i++){
+			
+			if(i%2!=0){
+				System.out.println("  " + i);
+				
+			}
+			
+		}
+		
+		int x = 0;
+		
+			do{
+			
+			System.out.println(x);
+			x = x + 2;
+			}while(x<=num);
+			
+			
+
+int j = 0;
+
+while(j<1){
+	
+	System.out.println("  *");
+
+	System.out.println(" ***");
+	System.out.println(" ");
+	System.out.println("*****");
+	System.out.println(" ");
+	System.out.println(" ***");
+	System.out.println(" ");
+	System.out.println("  *");
+	
+	j++;
+	
+	
+	
+}
+	
+	}
+	
+		
+		
+		
+	}
+		
+	
+
+
